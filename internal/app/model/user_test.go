127ce396e89c3f99859f90cda1247fc6393ca862
anastasia <petrichuk.nastya@yandex.ru>
2021-11-03 16:11:39 +0300

    добавлены валидации

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -6,6 +6,78 @@ import (
 	"testing"
 )
 
+func TestUserValidate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		u       func() *model.User
+		isValid bool
+	}{
+		{
+			name: "valid",
+			u: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+		{
+			name: "empty_email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "invalid_email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = "invalid@"
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "with_encrypt_password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				u.EncryptedPassword = "encryptedpasswordencryptedpasswordencryptedpassword"
+				return u
+			},
+			isValid: true,
+		},
+		{
+			name: "empty_password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "short_password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = "12 a"
+				return u
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.u().Validate())
+			} else {
+				assert.Error(t, tc.u().Validate())
+			}
+		})
+	}
+}
+
 func TestUser_BeforeCreate(t *testing.T) {
 	u := model.TestUser(t)
 
