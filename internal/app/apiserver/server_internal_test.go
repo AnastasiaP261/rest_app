cb08cf14b7a95fce852aef0f21c7422c6dbc0d17
anastasia <petrichuk.nastya@yandex.ru>
2021-11-04 16:56:51 +0300

    добавлены тесты сервера

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -1,1 +1,18 @@
 package apiserver
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"rest_app/internal/app/store/teststore"
+	"testing"
+)
+
+func TestServer_HadleUsersCreate(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New())
+	s.ServeHTTP(rec, req)
+
+	assert.Equal(t, rec.Code, http.StatusOK)
+}
